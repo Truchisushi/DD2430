6301c77b6f2a2c5b547a854afe9d3ceed6817a64
Trochez <trochezdaniel95@gmail.com>
2020-11-20 15:49:14 +0100

    Change to callable from other scripts

--- JustF1Score.py
+++ JustF1Score.py
@@ -5,34 +5,38 @@ import copy
 import math
 from PIL import Image
 
+def main():
+    ys = ['./data/validation_output/' + f for f in listdir('./data/validation_output/')]
+    results = np.load("result.npy")
+    f1_score = 0
+    for i in range(120):
+        validations = (np.asarray(Image.open(ys[i]))).astype(np.float32)
+        trueNegatives = 0
+        falseNegatives = 0
+        truePositives = 0
+        falsePositives = 0
+        for r in range(256):
+            for c in range(256):
+                if (results[i][r][c] == 0.0):
+                    if (np.any(validations[r][c] == 0.0)):
+                        trueNegatives = trueNegatives + 1
+                    else:
+                        falseNegatives = falseNegatives + 1
+
+                if (results[i][r][c] == 255.0):
+                    if (np.any(validations[r][c] == 255.0)):
+                        truePositives = truePositives + 1
+                    else:
+                        falsePositives = falsePositives + 1
+
+        precision = truePositives / (truePositives + falsePositives) if (truePositives + falsePositives) > 0 else 0
+        recall = truePositives / (truePositives + falseNegatives) if (truePositives + falseNegatives) > 0 else 0
+
+        if precision != 0 or recall != 0:
+            f1_score = f1_score + (2 * precision * recall / (precision + recall) / 120)
+
+    print("F1 Score:", f1_score)
+
 if __name__ == "__main__":
+    main()
 
-        ys = [ './data/validation_output/'+ f for f in listdir('./data/validation_output/')]
-        results = np.load("result_120x256x256.npy")
-        f1_score = 0
-        for i in range(120):
-            validations = (np.asarray(Image.open(ys[i]))).astype(np.float32)
-            trueNegatives = 0
-            falseNegatives = 0
-            truePositives = 0
-            falsePositives = 0
-            for r in range(256):
-                for c in range(256):
-                    if(results[i][r][c] == 0.0):
-                        if(np.any(validations[r][c] == 0.0)):
-                            trueNegatives = trueNegatives + 1
-                        else:
-                            falseNegatives = falseNegatives + 1
-
-                    if(results[i][r][c] == 255.0):
-                        if(np.any(validations[r][c] == 255.0)):
-                            truePositives = truePositives + 1
-                        else:
-                            falsePositives = falsePositives + 1
-
-            precision = truePositives / (truePositives + falsePositives)
-            recall = truePositives / (truePositives + falseNegatives)
-
-            f1_score = f1_score + (2*precision*recall / (precision + recall) / 120)
-
-        print(f1_score)
