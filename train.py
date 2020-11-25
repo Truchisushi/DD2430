a032b3d4abd3589d0ea900fd605d1eea73bacb93
Trochez <trochezdaniel95@gmail.com>
2020-11-25 15:26:52 +0100

    Train script

---
+++ train.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+def main():
+    """
+    Main part of the training. arguments sent in to main() to define training. Arguments could include:
+        - Epochs
+        - Batch size
+        - Verbosity
+    :return:
+    """
+    return 0
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("hej")
+
+
+
+
+
+
+
+
+
+
+
+
