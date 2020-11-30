a4213e76400118885b15c756030ecb4a068c1ec1
Trochez <trochezdaniel95@gmail.com>
2020-11-30 15:06:06 +0100

    update

--- Skeltonizer_Load.py
+++ Skeltonizer_Load.py
@@ -1,55 +1,35 @@
+import pickle
+
+import argparse
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
 from os import listdir
 from Skeltonizer_GCP import ImageDataSet, Skeltonizer
-from torch.utils.data import DataLoader, Dataset
-from JustF1Score import main
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from JustF1Score import compute_F1
 
-batch_size = 1  # batch size
-shuffle = False  # data augmentation shuffling
-epochs = 500  # number of epochs
-num_workers = 0
+"""
+This program loads a trained model to get results from. Data is loaded from "data.pickle" with keys:
+    'train_in', 'train_target', 'val_in', 'val_target', 'test_in', 'test_target'
 
+It has two main  purposes:
+    - Generate results for a given threshold
+    - Generate Plot for different thresholds
 
-# Save data
-xs = ['./data/img_train_shape/' + f for f in listdir('./data/img_train_shape/')]
-ys = ['./data/img_train2/' + f for f in listdir('./data/img_train2/')]
+Optional experiments can be added as functions to be computed by necessary flags
 
-xs_val = ['./data/validation_input/' + f for f in listdir('./data/validation_input/')]
-ys_val = ['./data/validation_output/' + f for f in listdir('./data/validation_output/')]
+"""
 
-train_data = ImageDataSet(xs, ys)
-val_data = ImageDataSet(xs_val, ys_val)
+def compute(model, model_name, data_loader, thresh =0.8):
+    """
+    compute outputs given dataset and threshold
+    return result array
+    """
 
-print("Configuring DataLoader for training set")
-train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-train_size = len(train_loader)
-print("Done.")
-
-print("Configuring DataLoader for Validation set")
-val_loader = DataLoader(dataset=val_data, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-val_size = len(val_loader)
-print("Done.")
-
-# create model, or load model
-s = 'focal_loss_expLRdecay096/models_e35'
-
-
-model = Skeltonizer()
-model.load_state_dict(torch.load(s))
-model.eval()
-#print(model)
-
-#print(torch.cuda.get_device_name() if torch.cuda.is_available() else "cpu")
-model.cuda()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
-for thresh in np.arange(0.6, 1.0, 0.01):
     count = 0
-    result_array = np.empty(shape=(val_size, 256, 256))
-    for data, target in val_loader:
+    result_array = np.empty(shape=(len(data_loader), 256, 256))
+    for data, target in data_loader:
         input = data.to(device)
         target = target.to(device)
 
@@ -62,12 +42,17 @@ for thresh in np.arange(0.6, 1.0, 0.01):
         p[p > thresh] = 255
         p[p <= thresh] = 0
         result_array[count, ...] = p.squeeze()
+
         f, axarr = plt.subplots(1, 3)
         axarr[0].imshow(p.squeeze(), cmap='gray')
+        axarr[0].set_title('Prediction')
         axarr[1].imshow(t, cmap='gray')
+        axarr[1].set_title('Target')
         axarr[2].imshow(i, cmap='gray')
-        #plt.savefig('train' + str(count) + '.png')
-        plt.show()
+        axarr[2].set_title('Input')
+        plt.savefig('./results/'+ model_name + '_thresh'+str(thresh)+'_pred'+ str(count) + '.png')
+        plt.close()
+        #plt.show()
         count += 1
         # p[torch.argmax(output, keepdim=True)] = 255
         # p[output<=0.9] = 0
@@ -77,7 +62,72 @@ for thresh in np.arange(0.6, 1.0, 0.01):
         # image.save("output_result.png", "PNG")
         # image.show()
         # time.sleep(1)
-    #print(result_array.shape)
-    np.save("result", result_array)
+    # print(result_array.shape)
+    #np.save("result", result_array)
     print("Threshold: " + str(thresh))
-    main()
+
+    return result_array
+
+def threshold_study(model, model_name, t_start, t_end, step, data_loader, targets):
+    """
+    Compute F1 score for a range of thresholds.
+    save figure for the curve of F1 score
+    """
+    #thresh_study
+    model.cuda()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+
+    thresholds = np.arange(t_start, t_end, step)
+    F1_scores = np.empty_like(thresholds)
+    #targets = data_loader
+    for i, thresh in enumerate(thresholds):
+        result = compute(model, model_name, data_loader, thresh)
+        F1_scores[i] = compute_F1(result, targets)
+
+
+    np.save("threshold_study_"+model_name, np.array([thresholds, F1_scores]))
+
+
+def main(args):
+    """
+    Perform the experiments that are flagged.
+    """
+    return 0
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Run experiments on trained model.")
+    parser.add_argument('-m', '--model', help='File path to the model')
+    parser.add_argument('thresholds', metavar='T', type=float, nargs='?')
+
+    args = parser.parse_args()
+    num_workers=0
+    batch_size = 1  # batch size
+
+    #Load the data
+    print("Loading Data...")
+    data = pickle.load(open("data.pickle", "rb"))
+    test_dataset = TensorDataset(torch.from_numpy(data["test_in"]).float(), torch.from_numpy(data["test_target"]).float())
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+    test_size = 1.0 * len(test_loader)
+    print("Done.")
+
+    #Load the model
+    s= args.model #'./models/models_e24_loss0.012'
+
+    model_name = s.replace('./models/', '')
+    model = Skeltonizer()
+    model.load_state_dict(torch.load(s))
+    model.eval()
+    print(torch.cuda.get_device_name() if torch.cuda.is_available() else "cpu")
+    model.cuda()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    #run whatever experiment we want.
+    t_start = 0.6
+    t_end = 1.0
+    t_step = 0.01
+    threshold_study(model, model_name, t_start, t_end, t_step, test_loader, data['test_target'].squeeze() * 255.0)
--- train.py
+++ train.py
@@ -1,16 +1,23 @@
-import sys
+import pickle
 
-if(len(sys.argv)<4 or len(sys.argv)>4):
-    print("Number of arguments are invalid")
-    
-epo = sys.argv[1]
-batch_size = sys.argv[2]
-verbosity = sys.argv[3]
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from matplotlib import pyplot as plt
+
+import time
+import argparse
+
+import numpy as np
+
+from torch.optim.lr_scheduler import ExponentialLR
+from torch.utils.data import TensorDataset, DataLoader
+
+from Skeltonizer_GCP import Skeltonizer, my_loss
 
 
-
-
-def main():
+def main(args):
     """
     Main part of the training. arguments sent in to main() to define training. Arguments could include:
         - Epochs
@@ -18,17 +25,118 @@ def main():
         - Verbosity
     :return:
     """
-    return 0
+    epochs = args.epochs
+    batch_size = args.batchsize
+    gamma = args.gamma
+    learning_rate = args.LR
+    print("Epochs: " + str(epochs) + ", Batch size: " + str(batch_size) + ", LR_decay: " + str(gamma) +", LR: "+ str(learning_rate))
+    num_workers = args.workers    #Change this to appropriate number of workers
 
+    # -------------------------------Data Loader--------------------------------------------
 
+    # Load Data
+    print("Loading Data...")
+    data = pickle.load(open("data.pickle", "rb"))
+    train_dataset = TensorDataset(torch.from_numpy(data["train_in"]).float(), torch.from_numpy(data["train_target"]).float())
+    val_dataset = TensorDataset(torch.from_numpy(data["val_in"]).float(), torch.from_numpy(data["val_target"]).float())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
+                              pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+    train_size = 1.0 * len(train_loader)
+    val_size = 1.0 * len(val_loader)
+    print("Done.")
 
+    # -----------------------------------create model, or load model------------------------------------
+    model = Skeltonizer()
+    # model.load_state_dict(torch.load('./models'))
+    model.eval()
+    print(model)
 
+    # --------------------------------------------------------------------------------------------------
+    print(torch.cuda.get_device_name() if torch.cuda.is_available() else "cpu")  # Make sure GPU is compatible with CUDA
+    model.cuda()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    e = np.arange(0, epochs)
+    train_losses = np.zeros(epochs)
+    val_losses = np.zeros(epochs)
+
+    # Adjust learning rate by multiplying LR-factor with lambda:
+
+    scheduler = ExponentialLR(optimizer, gamma=gamma)
+
+    # ---------------------------------------------------train--------------------------------------------
+    model.train()
+    best_val = np.finfo(float).max
+    for i in range(epochs):
+        start = time.time()
+        count = 0
+        # samples for training
+        #train_loss = 0.0
+        #val_loss = 0.0
+        for data, target in train_loader:
+            print('Epoch %d: %d/%d' % (i, count, train_size), end='\r')
+            data = data.to(device)
+            target = target.to(device)
+
+            optimizer.zero_grad()  # zero the gradient buffers
+            output = model(data)
+            loss = my_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            train_losses[i] += loss.item() / train_size
+
+            count += 1
+        print("")
+        # model.eval()
+        with torch.no_grad():
+            for data, target in val_loader:
+                data = data.to(device)
+                target = target.to(device)
+                output = model(data)
+                loss = my_loss(output, target)
+                val_losses[i] += loss.item() / val_size
+
+        end = time.time()
+        lr = 0
+        for param_group in optimizer.param_groups: lr = param_group['lr']
+        print("Epoch %d/%d, %d s, loss: %f, val: %f, lr: %f" % (
+        i, epochs, end - start, train_losses[i], val_losses[i], lr))
+
+        scheduler.step()
+
+        if (val_losses[i] < best_val):
+            print("Saving model!")
+            best_val = val_losses[i]
+            torch.save(model.state_dict(), './models/models_e' + str(i) + '_loss' + str(np.round(val_losses[i], 4)))
+        elif (i % 5 == 0): #save model when val_loss is improved, or every other epoch
+            print("Saving model!")
+            torch.save(model.state_dict(), './models/models_e' + str(i) + '_loss' + str(np.round(val_losses[i], 4)))
+    # save the model after the training
+    # torch.save(model.state_dict(), './models')
+
+    plt.plot(e, train_losses)
+    plt.plot(e, val_losses)
+    plt.legend(["train", "val"])
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.savefig("loss_plot.png")
+    plt.show()
 
 
 if __name__ == "__main__":
-    print("hej")
+    parser = argparse.ArgumentParser(description='Process to train U-net Skeltonizer. When running into memory issues, try lowering batchsize and(or) workers')
+    parser.add_argument('-e', '--epochs', type=int, help='Number of epochs', default=100)
+    parser.add_argument('-b', '--batchsize', type=int, help='Mini-batch size', default=16)
+    parser.add_argument('-g', '--gamma', type=float, help='Exponential Learning Rate Decay', default=0.96)
+    parser.add_argument('-l', '--LR', type=float, help='Learning Rate', default=0.0001)
+    parser.add_argument('-w', '--workers', type=int, help='Amount of concurrent workers.', default=4)
+
+    main(parser.parse_args())
 
 
 
